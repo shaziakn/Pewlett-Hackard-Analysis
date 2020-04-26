5b72515022844a11f556051b697c4c75dbb9e408
Shazia Nooruddin <shaziakn@Shazias-MacBook.local>
2020-04-26 13:37:07 -0500

    Troubleshooting

--- schema.sql
+++ schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
@@ -31,4 +31,20 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-SELECT * FROM departments;
+CREATE TABLE dept_emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR(200) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+SELECT * FROM dept_emp;
